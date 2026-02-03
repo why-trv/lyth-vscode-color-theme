a317e32d78623fd5ac44d1fb9dff700b1d8e74e4
why-trv <why.turov@gmail.com>
2026-02-03 16:33:05 +0400

    Add a slightly brighter variation to test drive

---
+++ src/defs/lyth-dark-lum.ts
@@ -0,0 +1,11 @@
+import type { ThemeDefinition } from "../types";
+
+const definition: ThemeDefinition = {
+  name: "Lyth Dark Lum Tweak",
+  extends: "Lyth Dark",
+  adjustments: {
+    highlights: 0.02,
+  },
+};
+
+export default definition;
